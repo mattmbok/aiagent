5387fdc3a952a92a0f26f4ff94a1854803919437
mattmbok <matthieu@mbock.dev>
2025-11-29 18:41:25 +0100

    added prompt to gemini flash with program argument as input

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+
+
 def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    if len(sys.argv) < 2:
+        print("no prompt provided")
+        sys.exit(1)
+    prompt = sys.argv[1]
+    response = client.models.generate_content(model="gemini-2.0-flash-001", 
+                                   contents=prompt)
     print("Hello from aiagent!")
+    print(response.text)
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
